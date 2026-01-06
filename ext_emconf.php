a45c92362aa275a7d2c78b08273b6bddce339cad
Sybille Peters <sybille.peters@uni-oldenburg.de>
2026-01-06 18:16:28 +0100

    Add support for v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'in2code.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.2.0-12.5.99',
+            'typo3' => '12.2.0-13.4.99',
             'php' => '8.1.0 - 8.3.99'
         ],
         'conflicts' => [
